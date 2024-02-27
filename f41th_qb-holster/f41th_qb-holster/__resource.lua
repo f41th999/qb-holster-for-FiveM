351aa748f5cb0dbfdd2fedda727b07abaae28d9d
f41th999 <161503355+f41th999@users.noreply.github.com>
2024-02-27 18:44:02 +0100

    Delete f41th_qb-holster/f41th_qb-holster directory

--- f41th_qb-holster/f41th_qb-holster/__resource.lua
+++
@@ -1,8 +0,0 @@
-resource_manifest_version "05cfa83c-a124-4cfa-a768-c24a5811d8f9"
-
-client_script 'client.lua'
-fx_version 'bodacious'
-game 'gta5'
-author 'Dr. Otacon x Slayzzz#0457'
-description 'An advanced Holster Script for QBCore Framework!'
-
