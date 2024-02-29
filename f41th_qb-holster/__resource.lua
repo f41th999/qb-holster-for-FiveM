cb5e4bb369f8b7c6998035ffb2e37deb3f918f9a
f41th999 <161503355+f41th999@users.noreply.github.com>
2024-02-29 09:22:54 +0100

    Update __resource.lua

--- f41th_qb-holster/__resource.lua
+++ f41th_qb-holster/__resource.lua
@@ -3,6 +3,6 @@ resource_manifest_version "05cfa83c-a124-4cfa-a768-c24a5811d8f9"
 client_script 'client.lua'
 fx_version 'bodacious'
 game 'gta5'
-author 'Dr. Otacon x Slayzzz#0457'
+author 'Dr. Otacon x f41th999'
 description 'An advanced Holster Script for QBCore Framework!'
 
